af0839c34f5c14d889903e2bb9d51f0590b8e776
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-07-30 13:02:55 +0530

    app dashboard changes

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full flex flex-col items-center h-full p-4">
+      <div className=" flex flex-col items-start w-full">
+        <p className=" text-2xl font-semibold text-black">Dashboard</p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
