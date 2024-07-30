1f83c1895cb302539b92845398fb84d851f0099f
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-07-30 13:02:00 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d2g3h1gpjmm5ra.cloudfront.net",
+      },
+    ],
+  },
+  //   transpilePackages: ["three"],
+  //   webpack: (config, { isServer }) => {
+  //     config.module.rules.push({
+  //       test: /\.obj$/,
+  //       use: [
+  //         {
+  //           loader: "file-loader",
+  //           options: {
+  //             name: "[name].[ext]",
+  //             outputPath: "static/models/",
+  //             publicPath: "/_next/static/models/",
+  //           },
+  //         },
+  //       ],
+  //     });
+
+  //     return config;
+  //   },
+};
 
 export default nextConfig;
