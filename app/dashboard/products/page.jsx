2f96c9640a269cae74d287083e251de77ff4cb3c
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-08-01 13:23:15 +0530

    added product view page

---
+++ app/dashboard/products/page.jsx
@@ -0,0 +1,30 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import useStore from "@/utils/store";
+import ProductDataTable from "./_productTable";
+
+const Products = () => {
+  const { showCreateProductModal, setShowCreateProductModal } = useStore();
+
+  return (
+    <div className="w-full flex flex-col items-center h-full p-4">
+      <div className=" flex flex-row justify-between w-full items-center">
+        <p className=" text-2xl font-semibold text-black">Products</p>
+
+        <button
+          onClick={() => {
+            setShowCreateProductModal({ show: true, id: "", mode: "create" });
+          }}
+          className="bg-black text-white px-4 py-2 rounded-md"
+        >
+          Add Product
+        </button>
+      </div>
+      <div className="w-full flex flex-col max-h-[70vh] mt-4 ">
+        <ProductDataTable />
+      </div>
+    </div>
+  );
+};
+
+export default Products;
