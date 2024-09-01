d4f3024a3f4918cb2b7579a16b0f7c05002fd1ec
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-09-01 11:12:31 +0530

    page and package changes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import setupLocatorUI from "@locator/runtime";
 
 export default function Home() {
   return <div></div>;
