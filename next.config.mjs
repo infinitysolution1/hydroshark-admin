8a0da3cd96fa2e1c67d934140a034c3f7844249f
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-09-07 22:56:22 +0530

    minor fix

--- next.config.mjs
+++ next.config.mjs
@@ -49,7 +49,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://api-prod.hydroshark.in/:path*/",
+        destination: "http://api-prod.hydroshark.in/:path*",
       },
     ];
   },
