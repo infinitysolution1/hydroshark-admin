5a6210c3a7a69d6abd30bb58e8a2e7707311952f
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-09-08 00:11:53 +0530

    minor fix

--- next.config.mjs
+++ next.config.mjs
@@ -45,15 +45,15 @@ const nextConfig = {
     ];
   },
 
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination:
-          "http://af08e05aaa6f34c1c9a51184d88204d6-1460487804.us-east-1.elb.amazonaws.com/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       destination:
+  //         "http://af08e05aaa6f34c1c9a51184d88204d6-1460487804.us-east-1.elb.amazonaws.com/:path*",
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
